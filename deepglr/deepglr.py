96d7f24753c67a41fa6b321d7aa028a24ba2a973
Huy D. Vu <huyvd7@gmail.com>
2020-08-02 00:54:12 -0400

    Update deepglr.py

--- deepglr/deepglr.py
+++ deepglr/deepglr.py
@@ -388,6 +388,7 @@ class GLR(nn.Module):
             E = E.unsqueeze(0)
             Y = Y.unsqueeze(0)
         E = E.view(E.shape[0], E.shape[1], img_dim ** 2)
+        print(Y.shape)
         #Y = Y.view(Y.shape[0], img_dim ** 2, 3)
 
         L = laplacian_construction(width=img_dim, F=E)
