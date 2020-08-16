e14b205a3ef96178627f9253909b41b2fcbd97a4
Huy D. Vu <huyvd7@gmail.com>
2020-08-16 06:19:21 -0400

    Update train_DGLR.py

--- train_DGLR.py
+++ train_DGLR.py
@@ -104,8 +104,6 @@ def main(args):
                 g = glr.glr1
                 with torch.no_grad():
                     histW = g(inputs, debug=1)
-                print("\tCNNF stats: ", g.cnnf.layer1[0].weight.grad.mean())
-                print("\tCNNU grads: ", g.cnnu.layer[0].weight.grad.mean())
                 with torch.no_grad():
                     us = g.cnnu(inputs)
                     print("\tCNNU stats: ", us.max().data,  us.mean().data,us.min().data)
@@ -129,8 +127,6 @@ def main(args):
             g = glr.glr1
             with torch.no_grad():
                 histW = g(inputs, debug=1)
-            print("\tCNNF stats: ", g.cnnf.layer1[0].weight.grad.mean())
-            print("\tCNNU grads: ", g.cnnu.layer[0].weight.grad.mean())
             with torch.no_grad():
                 us = g.cnnu(inputs)
                 print("\tCNNU stats: ", us.max().data,  us.mean().data,us.min().data)
