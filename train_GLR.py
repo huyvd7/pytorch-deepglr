2d88c179960ee27bc93f7c94dda5f2bd17fea12c
Huy D. Vu <huyvd7@gmail.com>
2020-08-16 23:13:04 -0400

    Update train_GLR.py

--- train_GLR.py
+++ train_GLR.py
@@ -107,7 +107,7 @@ def main(args):
             loss = criterion(outputs, labels)
             loss.backward()
             torch.nn.utils.clip_grad_norm_(gtv1_params, 1e2)
-            torch.nn.utils.clip_grad_norm_(gtv1_params, 1)
+            torch.nn.utils.clip_grad_norm_(gtv2_params, 1)
 
             optimizer.step()
 
