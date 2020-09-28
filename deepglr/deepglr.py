9f104295fbe7ea5a21051cd993f98f91113bf4fc
Huy D. Vu <huyvd7@gmail.com>
2020-09-28 00:27:59 -0400

    Update deepglr.py

--- deepglr/deepglr.py
+++ deepglr/deepglr.py
@@ -627,7 +627,7 @@ def qpsolve(L, u, y, Im):
     """
     Solve equation (2) using (6)
     """
-    xhat = torch.inverse(Im + u[:, None] * L)
+    xhat = torch.inverse(Im + u * L)
     print(xhat.shape, y.shape, u.shape, Im.shape, L.shape)
     return xhat @ y
 
